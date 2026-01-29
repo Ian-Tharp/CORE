84b6dbdbc87dfe90ceef2392dc87cfcba8eb0d59
Ian Tharp <praht09ian@gmail.com>
2026-01-29 08:51:57 -0600

    feat(ui): Add notification panel component

--- ui/core-ui/src/app/shared/index.ts
+++ ui/core-ui/src/app/shared/index.ts
@@ -1,6 +1,15 @@
 // Shared Components
 export { ChatWindowComponent } from './chat-window/chat-window.component';
 export { CommandInputBarComponent } from './command-input-bar/command-input-bar.component';
+export {
+  NotificationPanelComponent,
+  Notification,
+  NotificationType,
+  NotificationPriority,
+  NotificationGroup,
+  NotificationPanelConfig,
+  DEFAULT_NOTIFICATION_CONFIG
+} from './notification-panel';
 export { SideNavigationComponent } from './side-navigation/side-navigation.component';
 export { StatusIndicatorComponent, StatusType } from './status-indicator';
 export { TopNavigationComponent } from './top-navigation/top-navigation.component';
