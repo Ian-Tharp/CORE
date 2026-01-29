566270d7fd03ef0d64c6e45a5ea0ddf6e8a71934
Ian Tharp <praht09ian@gmail.com>
2026-01-29 08:33:13 -0600

    feat(ui): Add status indicator component

---
+++ ui/core-ui/src/app/shared/index.ts
@@ -0,0 +1,10 @@
+// Shared Components
+export { ChatWindowComponent } from './chat-window/chat-window.component';
+export { CommandInputBarComponent } from './command-input-bar/command-input-bar.component';
+export { SideNavigationComponent } from './side-navigation/side-navigation.component';
+export { StatusIndicatorComponent, StatusType } from './status-indicator';
+export { TopNavigationComponent } from './top-navigation/top-navigation.component';
+
+// Shared Services
+export { LoadingStateService } from './services/loading-state.service';
+export { UiNotifyService } from './services/ui-notify.service';
